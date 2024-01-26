def269d04b7d3542c3e811f07b63b2f61aea23e6
CHOUAIB <128982286+chouaib-at@users.noreply.github.com>
2024-01-26 23:17:08 +0100

    installing components

---
+++ my-react-app/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+import Sidebar from "./components/Sidebar/sidebar.jsx ";
+import Home from "./components/Home/Home.jsx";
+import About from "./components/About/About.jsx";
+import Services from "./components/Services/Services.jsx";
+
+const App = () => {
+  return <div>App</div>;
+};
+
+export default App;
---
+++ my-react-app/src/components/Services/Services.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Services.css";
+import Sidebar from "../Sidebar/sidebar";
+
+const Services = () => {
+  return <div>Services</div>;
+};
+
+export default Services;
