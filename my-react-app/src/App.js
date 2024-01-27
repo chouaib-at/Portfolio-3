badc56123379dc8ded21107d04f06f4555694b56
CHOUAIB <128982286+chouaib-at@users.noreply.github.com>
2024-01-27 01:36:34 +0100

    fixing some css

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -4,9 +4,30 @@ import Sidebar from "./components/Sidebar/sidebar.jsx ";
 import Home from "./components/Home/Home.jsx";
 import About from "./components/About/About.jsx";
 import Services from "./components/Services/Services.jsx";
+import Resume from "./components/sidebar/Resume/Resume";
+import Portfolio from "./components/Portfolio/Portfolio.jsx";
+import Pricing from "./components/Pricing/Pricing.jsx";
+import Testimonials from "./components/Testimonials/Testimonials.jsx";
+import Blog from "./components/Blog/Blog.jsx";
+import Contact from "./components/Contact/Contact.jsx";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Sidebar />
+      <main className="main">
+        <Home />
+        <About />
+        <Services />
+        <Resume />
+        <Portfolio />
+        <Pricing />
+        <Testimonials />
+        <Blog />
+        <Contact />
+      </main>
+    </>
+  );
 };
 
 export default App;
