c5208cd786714625274a95c1818ae3bf0dee9cf7
CHOUAIB <128982286+chouaib-at@users.noreply.github.com>
2024-01-28 23:48:13 +0100

    fixing some css

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./App.css";
-import Sidebar from "./components/Sidebar/sidebar.jsx ";
+import Sidebar from "./components/Sidebar/sidebar.jsx";
 import Home from "./components/Home/Home.jsx";
 import About from "./components/About/About.jsx";
 import Services from "./components/Services/Services.jsx";
-import Resume from "./components/sidebar/Resume/Resume";
+import Resume from "./components/Resume/Resume.jsx";
 import Portfolio from "./components/Portfolio/Portfolio.jsx";
 import Pricing from "./components/Pricing/Pricing.jsx";
 import Testimonials from "./components/Testimonials/Testimonials.jsx";
