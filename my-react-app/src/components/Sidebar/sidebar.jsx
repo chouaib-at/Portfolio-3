c0ef8e33f6ad20cc7bb8fc50761931ac37980b56
CHOUAIB <128982286+chouaib-at@users.noreply.github.com>
2024-02-01 23:51:11 +0100

    navbar

--- my-react-app/src/components/Sidebar/sidebar.jsx
+++ my-react-app/src/components/Sidebar/sidebar.jsx
@@ -12,40 +12,40 @@ const Sidebar = () => {
         <div className="nav__menu">
           <ul className="nav__list">
             <li className="nav__item">
-              <a href="#home" className="nav__link">
+              <a href="#Home" className="nav__link">
                 <i className="icon-home"></i>
               </a>
             </li>
 
             <li className="nav__item">
-              <a href="#about" className="nav__link">
+              <a href="#About" className="nav__link">
                 <i className="icon-user-following"></i>
               </a>
             </li>
 
             <li className="nav__item">
-              <a href="#services" className="nav__link">
+              <a href="#Services" className="nav__link">
                 <i className="icon-briefcase"></i>
               </a>
             </li>
 
             <li className="nav__item">
-              <a href="#resume" className="nav__link">
+              <a href="#Resume" className="nav__link">
                 <i className="icon-graduation"></i>
               </a>
             </li>
             <li className="nav__item">
-              <a href="#portfolio" className="nav__link">
+              <a href="#Portfolio" className="nav__link">
                 <i className="icon-layers"></i>
               </a>
             </li>
             <li className="nav__item">
-              <a href="#blog" className="nav__link">
+              <a href="#Blog" className="nav__link">
                 <i className="icon-note"></i>
               </a>
             </li>
             <li className="nav__item">
-              <a href="#contact" className="nav__link">
+              <a href="#Contact" className="nav__link">
                 <i className="icon-bubble"></i>
               </a>
             </li>
