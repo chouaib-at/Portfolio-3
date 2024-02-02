7500332e3325c012b77390b5bd345eb0c02174f4
CHOUAIB <128982286+chouaib-at@users.noreply.github.com>
2024-02-02 17:39:43 +0100

    fixing the body

---
+++ my-react-app/src/components/Home/HeaderSocials.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const HeaderSocials = () => {
+  return (
+    <div className="home__socials">
+      <a
+        href="https://www.instagram.com/chou_003/"
+        className="home__socials-link"
+        target="__bank"
+      >
+        <i class="fa-brands fa-instagram"></i>
+      </a>
+
+      <a
+        href="https://www.linkedin.com/in/atrous-chouaib/"
+        className="home__socials-link"
+        target="__bank"
+      >
+        <i class="fa-brands fa-linkedin"></i>
+      </a>
+
+      <a
+        href="https://github.com/chouaib-at"
+        className="home__socials-link"
+        target="__bank"
+      >
+        <i class="fa-brands fa-github"></i>
+      </a>
+    </div>
+  );
+};
+
+export default HeaderSocials;
--- my-react-app/src/components/Home/Home.jsx
+++ my-react-app/src/components/Home/Home.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import "./Home.css";
+import avatar from "../../assets/6284034.png";
+import HeaderSocials from "./HeaderSocials";
+import ScrollDown from "../Home/ScrollDown";
 
 const Home = () => {
-  return <div className="home"> </div>;
+  return (
+    <section className="home-container" id="home">
+      <div className="intro">
+        <img src={avatar} alt="avatar" className="home__img" />
+        <h1 className="home__name">Chouaib Atrous</h1>
+        <span className="home__education">I am a Front-End Developer</span>
+        <HeaderSocials />
+        <a href="#contact" className=" btn">
+          Hire Me
+        </a>
+        <ScrollDown />
+      </div>
+    </section>
+  );
 };
 
 export default Home;
