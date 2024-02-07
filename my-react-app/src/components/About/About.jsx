c25996b0b69ae9b3c348c2c7661b2490ceeb16f4
CHOUAIB <128982286+chouaib-at@users.noreply.github.com>
2024-02-07 10:05:32 +0100

    s

--- my-react-app/src/components/About/About.jsx
+++ my-react-app/src/components/About/About.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./About.css";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section className="about container section" id="about ">
+      <h2 className="section__title">About Me</h2>
+    </section>
+  );
 };
 
 export default About;
