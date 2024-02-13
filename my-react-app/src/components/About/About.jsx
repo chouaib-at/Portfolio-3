7598e55cdc622657f727540b69a0aaa28f659cb9
CHOUAIB <128982286+chouaib-at@users.noreply.github.com>
2024-02-13 22:09:10 +0100

    fixing error

--- my-react-app/src/components/About/About.jsx
+++ my-react-app/src/components/About/About.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "./About.css";
-import image from "../../assets/images/";
+// import image from "../../assets/images/";
 
 const About = () => {
   return (
     <section className="about container section" id="about ">
       <h2 className="section__title">About Me</h2>
       <div className="about__container grid">
-        <img src={image} alt="" className="about__img" />
+        {/* <img src={image} alt="" className="about__img" /> */}
       </div>
     </section>
   );
